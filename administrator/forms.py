01e6327fe22b55ff9bf81efc69db8ddab5dbb5aa
Lofectr <dperelygin0@gmail.com>
2020-02-10 19:51:14 +0300

    ошибка! не изменяется переменная current в шаблоне(при чем в views.py она изменяется

--- administrator/forms.py
+++ administrator/forms.py
@@ -1,5 +1,1 @@
 from django import forms
-from .choice import ACTION_ADMIN_CHOICE
-
-class SelectAction(forms.Form):
-	select = forms.ChoiceField(choices=ACTION_ADMIN_CHOICE)
--- administrator/views.py
+++ administrator/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render,redirect
 from index.global_context import isAuth
-from .forms import SelectAction
 
 def administrator(request):
 	user = isAuth(request.session.get('email',None), request.session.get('password', None))
-	current = "0"
 	if user and user.role=="Admin":
-		if request.POST:
-			form = SelectAction(request.POST)
-			current = request.POST.get('select')
-		else:
-			form = SelectAction()
-		return render(request, 'administrator.html', {'form':form, 'current':current})
+		print(request.POST.get('current') or "0")
+		return render(request, 'administrator.html', {'current':request.POST.get('current') or "0"})
 	else:
 		return redirect('/')
