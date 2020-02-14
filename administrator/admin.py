008830b81dcf2e8728a1b491277f518fb8cb0a79
Lofectr <dperelygin0@gmail.com>
2020-02-14 21:37:49 +0300

    Генерация ссылки

--- administrator/admin.py
+++ administrator/admin.py
@@ -4,3 +4,4 @@ from .models import *
 admin.site.register(School)
 admin.site.register(Test)
 admin.site.register(Question)
+admin.site.register(SchoolClass)
--- createRefForTesting/views.py
+++ createRefForTesting/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from administrator.choice import REF_GENERATE_SIMBOL
+from random import choice
+from login.models import Teacher
+from index.global_context import *
 
 def createRefForTesting(request):
-	return render(request, 'createRefForTesting.html')
+	context = {}
+	user = isReg(request.session.get('email',None), request.session.get('password', None)) #пользователь
+	if user and isinstance(user, Teacher):
+		if user.classSchool.ref:
+			ref = user.classSchool.ref
+			context['ref'] = ref
+		if 'generateRefButton' in request.POST:
+			while True:
+				ref = ''
+				for i in range(30):
+					ref += choice(REF_GENERATE_SIMBOL)
+				try:
+					Teacher.objects.get(ref=ref)
+				except:
+					user.classSchool.ref = ref
+					user.classSchool.save()
+					context['ref'] = ref
+					break
+		context['titlePage'] = 'Создание ссылки'
+		return render(request, 'createRefForTesting.html', context)
+	else:
+		return redirect('/')
